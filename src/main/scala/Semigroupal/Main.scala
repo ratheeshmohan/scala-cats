e90b1cc705303398229a6a1933e4325ce447d623
rmohan <ratheeshmohan@gmail.com>
2018-12-29 08:08:46 +1100

    semigroupal

--- src/main/scala/Semigroupal/Main.scala
+++ src/main/scala/Semigroupal/Main.scala
@@ -1,7 +1,6 @@
 package Semigroupal
 
 import cats.Semigroupal
-
 import cats.instances.option._ // for Semigroupal
 
 object Main extends App {
@@ -38,10 +37,41 @@ object Main extends App {
 
 
   import cats.Monoid
+  //import cats.instances.all._
+  import cats.instances.string._
+  import cats.instances.int._
+  import cats.instances.list._
+  import cats.syntax.apply._         // for imapN
+  import cats.instances.invariant._  // for Semigroupal
+
   implicit val catMonoid: Monoid[Cat] = (
     Monoid[String],
     Monoid[Int],
     Monoid[List[String]]
   ).imapN(tupleToCat)(catToTuple)
+
+  import cats.Semigroupal
+  // import cats.instances.list._ // for Semigroupal
+
+  // val res1 = Semigroupal[List].product(List(1, 2), List(3, 4))
+  val res1 = (List(1, 2), List(3, 4)).tupled
+  println(res1)
+
+
+  import cats.Semigroupal
+  import cats.data.Validated
+  import cats.instances.list._ // for Monoid
+
+  type AllErrorsOr[A] = Validated[List[String], A]
+
+  val res2 = Semigroupal[AllErrorsOr].product(
+    Validated.invalid(List("Error 1","w")),
+    Validated.invalid(List("Error 2")))
+  println(res2)
+
+  val res3 = (Validated.invalid(List("Error 1", "r")),Validated.invalid(List("Error 9")),
+    Validated.invalid(List("Error 2"))).tupled
+  println(res3)
+
 
 }
